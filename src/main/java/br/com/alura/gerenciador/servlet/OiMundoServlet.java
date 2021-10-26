12e7a2a4285ebebe23586c01f300473af7e05df2
allayzon <allyson.g.d.santos@gmail.com>
2021-10-25 21:41:14 -0300

    Commit oficial

---
+++ src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,31 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/oi")
+public class OiMundoServlet extends HttpServlet{
+	
+	public OiMundoServlet() {
+		System.out.println("Testando o Tomcat container");
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("oi mundo, parabens vc escreveu o primeiro servlets.");
+		out.println("</body>");
+		out.println("</html>");
+
+		System.out.println("O servlet OiMundoServlet foi chamado");
+	}
+}
